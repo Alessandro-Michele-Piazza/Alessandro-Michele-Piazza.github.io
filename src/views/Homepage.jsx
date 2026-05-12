11bc24a20ebda1917d8e313881426fc2aabbecf3
Alessandro <alexpiazza98@gmail.com>
2026-05-12 14:03:34 +0200

    tentativo 2 alzamento cose nella navbar da telefono

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -49,7 +49,7 @@ export default function Homepage() {
         </div>
         <div>
           <img
-            src="/foto_mia_3.webp"
+            src="/foto_mia_4.webp"
             alt="Foto di Alessandro Piazza"
             className="mobile-intro__image"
             loading="lazy"
